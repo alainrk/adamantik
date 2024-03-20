722da18cb6c1502060650253ba26600a8dceb6dd
Alain <alain.devcs@gmail.com>
2024-03-20 07:21:15 +0100

    restrict users select field

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
+import { Prisma } from "@prisma/client";
 
 type UsersRequest = FastifyRequest<{
   Querystring: { cursor?: number; take?: number };
@@ -11,16 +12,20 @@ type UserRequest = FastifyRequest<{
 export default async function users(app: FastifyInstance) {
   app.get("/users", async (req: UsersRequest, res) => {
     const { cursor, take } = req.query;
-    const opts = {
-      cursor: { id: Number(cursor) },
-      skip: !!cursor ? 1 : 0,
+    const select: Prisma.UserSelect = {
+      name: true,
+      email: true,
+      createdAt: true,
+    };
+    const opts: Prisma.UserFindManyArgs = {
       take: Number(take) || 10,
       orderBy: { id: "asc" },
+      select,
     };
-    // TODO: Solve these two TS issues
-    // @ts-ignore
-    if (!cursor) delete opts.cursor;
-    // @ts-ignore
+    if (cursor) {
+      opts.cursor = { id: Number(cursor) };
+      opts.skip = !!cursor ? 1 : 0;
+    }
     const users = await app.prisma.user.findMany(opts);
     const newCursor = users[users.length - 1]?.id;
     return { users, cursor: newCursor };
@@ -28,8 +33,14 @@ export default async function users(app: FastifyInstance) {
 
   app.get("/users/:id", async (req: UserRequest, res) => {
     const { id } = req.params;
+    const select: Prisma.UserSelect = {
+      name: true,
+      email: true,
+      createdAt: true,
+    };
     return await app.prisma.user.findUniqueOrThrow({
       where: { id: Number(id) },
+      select,
     });
   });
 }
